164aaaf9da9efce50bc771eac9b1ebd62be437de
magicxin <x.yu2000@163.com>
2023-06-05 22:55:40 +0800

    v1.0.0.0

---
+++ DecryptionMLM/Decryption.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DecryptionMLM
+{
+    internal static class Decryption
+    {
+        private static Encoding encoding = Encoding.UTF8;
+
+        public static string DecryptDES(string decryptString, string key)
+        {
+            decryptString = decryptString.Replace('@', '/');
+            byte[] bytes = ProcessDES(Convert.FromBase64String(decryptString), key, false);
+            return encoding.GetString(bytes);
+        }
+
+        private static byte[] ProcessDES(byte[] data, string key, bool isEncrypt)
+        {
+            using (DESCryptoServiceProvider cryptoServiceProvider = new DESCryptoServiceProvider())
+            {
+                byte[] array1 = Md5(key);
+                byte[] array2 = new ArraySegment<byte>(array1, 0, 8).ToArray<byte>();
+                byte[] array3 = new ArraySegment<byte>(array1, 8, 8).ToArray<byte>();
+                ICryptoTransform transform = isEncrypt ? cryptoServiceProvider.CreateEncryptor(array2, array3) : cryptoServiceProvider.CreateDecryptor(array2, array3);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream) memoryStream, transform, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(data, 0, data.Length);
+                        cryptoStream.FlushFinalBlock();
+                        return memoryStream.ToArray();
+                    }
+                }
+            }
+        }
+
+        public static byte[] Md5(string str)
+        {
+            using (MD5 md5 = MD5.Create())
+                return md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+        }
+        
+        public static string De(string src)
+        {
+            if (src.StartsWith("mlm-"))
+            {
+                src = src.Remove(0, 4);
+                src = DecryptDES(src, "mlm");
+            }
+            return src;
+        }
+        
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please enter encrypted file path");
+                return;
+            }
+            string encryptedFilePath = args[0];
+            string decryptedFilePath = encryptedFilePath + "-decrypted.xml";
+            
+            string encryptedXml = File.ReadAllText(encryptedFilePath);
+            string decryptedXml = De(encryptedXml);
+            
+            File.WriteAllText(decryptedFilePath, decryptedXml);
+            Console.WriteLine("Decrypted XML file saved: " + decryptedFilePath);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+﻿# DecryptionMLM
+
+由于莫灵喵违反开源协议私自修改并发布私人版本的 Triggernometry 插件，并加入加密功能
+
+此工具提供了对加密后 xml 文件的解密，可以正常导入至原版插件
+
+<b>不保证你看到这个工具的时候它是可用的，不保证后续更新<b/>
+
+---
+
+## 使用方法
+
+1. 去 [release](https://github.com/Magic-Xin/DecryptionMLM/release) 下载最新版本的 exe 文件
+2. 运行 `DecryptionMLM your_file_path.xml`
+
+---
+
+## 编译需求
+
+.Net Framework 4.6.2
